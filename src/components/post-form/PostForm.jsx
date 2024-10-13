35afc5b7eaa9d4b186ae1ad54f4aece260055fb5
ravidangi7284 <93817949+ravidangi7284@users.noreply.github.com>
2024-10-13 15:24:45 +0530

    postform updated

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -71,8 +71,8 @@ export default function PostForm({ post }) {
     }, [watch, slugTransform, setValue]);
 
     return (
-        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap">
-            <div className="w-2/3 px-2">
+        <form onSubmit={handleSubmit(submit)} className="flex flex-wrap flex-col sm:flex-row">
+            <div className="w-full sm:w-2/3 px-2">
                 <Input
                     label="Title :"
                     placeholder="Title"
@@ -90,7 +90,7 @@ export default function PostForm({ post }) {
                 />
                 <RTE label="Content :" name="content" control={control} defaultValue={getValues("content")} />
             </div>
-            <div className="w-1/3 px-2">
+            <div className=" w-full sm:w-1/3 px-2">
                 <Input
                     label="Featured Image :"
                     type="file"
