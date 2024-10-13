730975edd7ac057d35f139d45556689bda6dedf4
ravidangi7284 <93817949+ravidangi7284@users.noreply.github.com>
2024-10-13 15:29:35 +0530

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -39,7 +39,7 @@ function Header() {
   return (
     <header className="py-3 m-10 shadow bg-gray-950 flex justify-center">
       <div>
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-gray-900 dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
             <Link
               to={"/"}
